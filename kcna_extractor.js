240f49210f98f866a01570bb7e299d8e0702ff86
syakante <SKim@csis.org>
2023-05-19 17:04:39 -0400

    updated kcna extractor

--- kcna_extractor.js
+++ kcna_extractor.js
@@ -3,7 +3,9 @@ const { extract, getSanitizeHtmlOptions, setSanitizeHtmlOptions } = require('@ex
 const { parseArgs } = require('node:util');
 const path = require('node:path');
 const fs = require('fs');
-const { parse } = require('csv-parse');
+const csv = require('csv');
+// const { parse } = require('csv-parse');
+// const { createObjectCsvWriter } = require('csv-writer');
 
 //const filepath = process.argv.slice(2)[0]; //aiieeeee
 
@@ -19,57 +21,92 @@ const mySanitizeOptions = {
 }
 const rightTag = /<\/(?:"[^"]*"['"]*|'[^']*'['"]*|[^'">])+>/g;
 const leftTag = /<(?:"[^"]*"['"]*|'[^']*'['"]*|[^'">])+>/g;
+const kncaRegex = /\(평양\s*\d+월\s*\d+일발\s*조선중앙통신\)(?:\s*\d+일부\s*)?(.*?)\(끝\)/;
 setSanitizeHtmlOptions(mySanitizeOptions);
 
 const filepath = 'test.csv';
 const header = false;
+//for now lets just assume the csv will be a single column. You can future-proof (is that the word?) later with argparse and stuff.
+//Just get this working for now aieeeeee
 
 function readCSV() {
   return new Promise((resolve, reject) => {
     const ret = []
     fs.createReadStream(path.join(__dirname, filepath))
-      .pipe(parse({ delimiter: ',', from_line: + header+1, encoding: 'utf8' }))
+      .pipe(csv.parse({ delimiter: ',', from_line: + header+1, encoding: 'utf8' }))
       .on("data", (row) => {
         ret.push(row)
       }).on('error', (err) =>  {
         console.error(err);
         reject(err);
       }).on("end", () => {
-      console.log(`read CSV, got ${ret.length} rows`)
-      resolve(ret);
+      console.log(`read CSV, got ${ret.length} rows`);
+      resolve(ret.flat().map(s => s.normalize().trim()));
       });
 
   })
 
 }
 
+async function getUrlArticleText(input) {
+  try {
+      const article = await extract(input)
+      //console.log(article)
+      if(article != null){
+        //console.log(`article title is ${article.title}`);
+        //rawContent = sanitizeHtml(article.content, { allowedTags: myAllowedTags });
+        //console.log(getSanitizeHtmlOptions())
+        articleText = article.content.replace(rightTag, '').replace(leftTag, ' ');
+        const matches = articleText.match(kncaRegex);
+        if(matches) {
+          return(matches[1])
+        } else {
+          console.log("Didn't find a regex match?!");
+          return('');
+        }
+        //console.log(articleText);
+        return(articleText[0]);
+      } else {
+        console.log("Got null. Seems like the webpage needs be rendered with JS client-side.");
+      }
+    } catch (err) {
+      console.error(`Error on ${input}: ${err}`);
+    }
+}
+
+//TODO: this doesn't work. aiieeeeee
+function writeCSV(filePath, data) {
+  return new Promise((resolve, reject) => {
+    const stream = csv.stringify({ header: true });
+    const writableStream = fs.createWriteStream(filePath);
+
+    writableStream.on('error', (err) => {
+      reject(err);
+    });
+
+    stream.pipe(writableStream).on('finish', () => {
+      resolve();
+    });
+
+    data.forEach((row) => {
+      stream.write(row);
+    });
+
+    stream.end();
+  });
+}
+
 async function main() {
   try {
-    const data = await readCSV();
-    console.log(data);
-    //do stuff
+    const urlArr = await readCSV();
+    const textArr = await Promise.all(urlArr.map((url) => getUrlArticleText(url)));
+    console.log("ok...");
+    //console.log(textArr);
+    await writeCSV('output.csv', textArr);
+    console.log("ajlsdkfjlsakdfj;sadlkfjkl");
   } catch (err) {
     console.error(err)
   }
 }
 
 main();
-
-async function getUrlArticleText() {
-  try {
-      const article = await extract(input)
-      //console.log(article)
-      if(article != null){
-        console.log(`article title is ${article.title}`);
-        //rawContent = sanitizeHtml(article.content, { allowedTags: myAllowedTags });
-        //console.log(getSanitizeHtmlOptions())
-        articleText = article.content.replace(rightTag, '').replace(leftTag, ' ');
-        console.log(articleText);
-        return
-      } else {
-        console.log("Got null. Seems like the webpage needs be rendered with JS client-side.");
-      }
-    } catch (err) {
-      console.error(err);
-    }
-}
